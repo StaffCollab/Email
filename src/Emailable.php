a237dc38bde7bc30260bd18aea50bc01cd9af629
StaffCollab <111918553+StaffCollab@users.noreply.github.com>
2025-07-08 16:06:56 +0000

    Fix styling

--- src/Emailable.php
+++ src/Emailable.php
@@ -5,11 +5,16 @@ namespace StaffCollab\Email;
 interface Emailable
 {
     public function getRecipients(): array;
+
     public function getAttachments(): array;
+
     public function getTemplateData(): array;
 
     public static function getName(): string;
+
     public static function getRecipientOptions(): array;
+
     public static function getAttachmentOptions(): array;
+
     public static function getMergeTags(): array;
 }
